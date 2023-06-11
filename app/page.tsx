2bf0f098407d3294125978aa8cdf57695e8deccb
ethereal element <vitalik.safonov@list.ru>
2023-06-11 14:06:39 +0300

    create: logo ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import {Button} from "@/app/components/ui/button/Button";
+import {Logo} from "@/app/components/ui/logo/Logo";
 
 export default function Home() {
     return (
         <main>
             <h1>Hello</h1>
             <Button>{"Связаться с разработчиком"}</Button>
+            <Logo></Logo>
             <div className="background-animation">
                 <div id="ball-one"></div>
                 <div id="ball-two"></div>
