ffd83edba5d3faad38ae30be783f090094da65d0
ethereal element <vitalik.safonov@list.ru>
2023-06-12 11:38:32 +0300

    create: burger menu

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,15 +1,34 @@
-import {FC} from 'react';
+"use client"
+import {FC, useState} from 'react';
 import {Logo} from "@/app/components/ui/logo/Logo";
 import styles from "./Header.module.scss";
 import Link from "next/link";
 import "../../globals.css";
+import {useMediaQuery} from "@/app/hooks";
 
 export const Header: FC = () => {
+    const isMobile = useMediaQuery(640);
+    const [menuOpen, setMenuOpen] = useState(false)
+    const handleToggleMenu = () => {
+        (document.querySelector("body") as HTMLBodyElement).classList.toggle("overflow-hidden");
+        setMenuOpen(!menuOpen);
+    }
+
+    const closeMenu = () => {
+        (document.querySelector("body") as HTMLBodyElement).classList.toggle("overflow-hidden");
+        setMenuOpen(false);
+    }
+
     return (
         <>
             <header className={styles.header}>
                <div className={`container ${styles.headerContainer}`}>
                    <Logo></Logo>
+                   {isMobile && <button onClick={handleToggleMenu} className={`${styles.burgerMenu} ${menuOpen ? styles.open : ''}`}>
+                       <span></span>
+                       <span></span>
+                       <span></span>
+                   </button>}
                    <nav className={styles.headerContainerNav}>
                        <ul className={styles.headerContainerNavList}>
                            <li className={styles.headerContainerNavListItems}>
--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,42 +1,38 @@
-"use client"
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react"
 
 const getWindowWidth = () => {
-    const {innerWidth: windowWidth} = typeof window !== undefined ? window : {innerWidth: 0}
+    const { innerWidth: windowWidth } = typeof window !== 'undefined'
+        ? window
+        : { innerWidth: 0 }
 
-    return {
-        windowWidth
-    }
+    return { windowWidth }
 }
 
 const useWindowWidth = () => {
-    const [windowWidth, setWindowWidth] = useState(getWindowWidth);
+    const [windowWidth, setWindowWidth] = useState(getWindowWidth());
 
     const handleResize = () => setWindowWidth(getWindowWidth());
 
-    useEffect(()=> {
-        window.addEventListener("resize", handleResize)
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
 
-        return ()=> window.removeEventListener("resize", handleResize);
-    },[])
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
-    return [windowWidth, handleResize];
+    return { windowWidth, handleResize };
 }
 
 export const useMediaQuery = (maxWidth: number) => {
-    const {windowWidth: {windowWidth}, hanldeResize} = useWindowWidth();
+    const { windowWidth: { windowWidth }, handleResize } = useWindowWidth();
     const [isMedia, setIsMedia] = useState(false);
 
-    useEffect(()=> {
-        hanldeResize();
-
+    useEffect(() => {
         if (windowWidth <= maxWidth) {
             setIsMedia(true);
         } else {
-            setIsMedia(false)
+            setIsMedia(false);
         }
-    },[handleResize, maxWidth, windowWidth]);
-
+    }, [handleResize, maxWidth, windowWidth]);
+    
     return isMedia;
 }
-
