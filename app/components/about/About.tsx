21e19b81251087fc49bce52f7ac4128a194292f9
ethereal element <vitalik.safonov@list.ru>
2023-06-14 10:34:22 +0300

    create arrow ui component

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,13 +1,28 @@
 import { FC } from 'react';
 import styles from "./About.module.scss"
 import {Title} from "@/app/components/ui/title/Title";
+import Image from "next/image";
 export const About: FC = () => {
     return <section className={styles.about} id="about">
         <div className="container">
         <Title>ОБО МНЕ</Title>
         </div>
-        <div className="sub-container">
-
+        <div className={`"sub-container" ${styles.aboutSubContainer}`}>
+            <Image
+            src="/avatar.png"
+            width={552}
+            height={557}
+            alt="avatar"
+            className={styles.aboutSubContainerImg}
+            />
+            <div className={styles.aboutSubContainerInner}>
+                <h3>
+                    <span className={styles.aboutSubContainerInnerBorder}>
+                        <span className={styles.aboutSubContainerInnerBorderArrow}></span>
+                        <span className={styles.aboutSubContainerInnerBorderLine}></span>
+                    </span>
+                </h3>
+            </div>
         </div>
     </section>;
 };
