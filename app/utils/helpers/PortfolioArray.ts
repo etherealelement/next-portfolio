8232a965111e72990063ea1bb5cb3716c2163c23
ethereal element <vitalik.safonov@list.ru>
2023-06-19 20:59:54 +0300

    fix: url safari

--- app/utils/helpers/PortfolioArray.ts
+++ app/utils/helpers/PortfolioArray.ts
@@ -11,5 +11,5 @@ export const PortfolioArray: iPortfolioArray[] = [
     {id: 2, description: "Интернет-магазин кроссовок", imgUrl: "/react-sneakers.png", link: "https://etherealelement.github.io/sneakers-app/"},
     {id: 3, description: "Сайт-визитка Mersedes-AVTR", imgUrl: "/avatar-page.png", link: "https://etherealelement.github.io/AVTR/"},
     {id: 4, description: "Сайт-визитка элекрических велосипедов Xiaomi-Himo", imgUrl: "/himo.png", link: "https://etherealelement.github.io/XiaomiHimo/"},
-    {id: 5, description: "Сайт туристического агенства", imgUrl: "/safari.png", link: "https://64843dccd2a28e3e234b0d42--coruscating-concha-ad2f70.netlify.app/"},
+    {id: 5, description: "Сайт туристического агенства", imgUrl: "/safari.png", link: "https://etherealelement.github.io/safari/"},
 ]
